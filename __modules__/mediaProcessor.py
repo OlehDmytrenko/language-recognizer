1f1236bb0ee86303f55eeff71332532383577420
Oleh Dmytrenko <dmytrenko.o@gmail.com>
2022-07-01 20:04:32 +0300

    v2.2
    Add config file
    Add time for file name
    Add print file name
    Rename some modules
    Update README

--- __modules__/mediaProcessor.py
+++ __modules__/mediaProcessor.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 26 19:43:43 2022
-
 @author: dmytrenko.o
 """
 import sys
@@ -13,30 +11,38 @@ sys.stdout = stdOutput
 import os
 from scipy.io import wavfile
 import moviepy.editor as mp
+import shutil
 
-def audio2parts(audiosDir, sec):
-    partsAudioDir = audiosDir+'part_audios/'
-    if not os.path.exists(partsAudioDir):
-        print ("Directory {0} don't exist!".format(partsAudioDir))
-        print ("Creating {0} directory...".format(partsAudioDir))
-        os.mkdir(partsAudioDir)
-        print ("Directory {0} was created seccsesfuly!".format(partsAudioDir))
-        
-    name = 0
+def name_time_stamp(timeStamp, audioDuration):
+    start_hours = int(timeStamp/3600)
+    start_mins = int(timeStamp/60)
+    start_sec = int(timeStamp%60)
+    end_hours = int((timeStamp+audioDuration)/3600)
+    end_mins = int((timeStamp+audioDuration)/60)
+    end_sec = int((timeStamp+audioDuration)%60)
+    name = "{0}_{1}_{2}__{3}_{4}_{5}".format(start_hours, start_mins, start_sec, end_hours, end_mins, end_sec)
+    timeStamp += audioDuration
+    return name, timeStamp
+    
+
+def audio2parts(audiosDir, audioDuration):
+    timeStamp = 0
     for file_ in os.listdir(audiosDir+'full_audios/'):
         samplerate, data = wavfile.read(audiosDir+'full_audios/'+file_)
         print ('Samplerate {0}'.format(samplerate))
         totalSize = len(data)
-        ChunkSize = samplerate*sec #1 minute has 60 sec
+        ChunkSize = samplerate*audioDuration #1 minute has 60 sec
         amountChunk = int(totalSize/ChunkSize)
         
         for i in range(amountChunk):
             chunkData = data[i*ChunkSize:i*ChunkSize+ChunkSize]
-            name += 1
-            wavfile.write(partsAudioDir+str(name)+'.wav', samplerate, chunkData)
-    return partsAudioDir
+            name, timeStamp = name_time_stamp(timeStamp, audioDuration)
+            wavfile.write(audiosDir+str(name)+'.wav', samplerate, chunkData)
+    shutil.rmtree(audiosDir+'full_audios/')
+    return
 
 def video2wav(videosDir, audiosDir):
+    shutil.rmtree(audiosDir)
     if not os.path.exists(audiosDir):
             print ("Directory {0} don't exist!".format(audiosDir))
             print ("Creating {0} directory...".format(audiosDir))
